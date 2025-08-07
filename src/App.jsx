402764080b8fc521c30e59f83422717fd122d48f
Vishnu Kalanad <dxbvishnu@gmail.com>
2025-08-07 17:21:40 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-stone-800 text-xl">You got this 💪</h1>
+      <p>Stuck? Not sure how to proceed?</p>
+      <p>Don't worry - we've all been there. Let's build it together!</p>
+    </>
+  );
+}
+
+export default App;
